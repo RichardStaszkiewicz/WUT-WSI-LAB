cfcefde35aa9c0b065edfd33ab5a951a8ecb4fa3
Richard <richard.staszkiewicz@gmail.com>
2022-01-16 20:08:34 +0100

    Fully functional algorithm with a plotter

--- Reinforcement-Learning/QLearn.py
+++ Reinforcement-Learning/QLearn.py
@@ -1,8 +1,7 @@
-from cgitb import small
-from genericpath import samefile
-from turtle import pen
+from math import gamma
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class QLearn:
@@ -15,19 +14,24 @@ class QLearn:
         # logs of simulations for training
         self.simulation_training_log = [(0, 0)] # epochs, penalties
 
-    def train(self, alpha=None, gamma=None, expl=None, epochs=None):
+    def train(self, alpha=None, gamma=None, expl=None, epochs=None, simulations=None):
         self.EPOCHS = 1000000 if epochs is None else epochs
         self.learning_rate_alpha = 0.1 if alpha is None else alpha
-        self.discount_factior_gamma = 0.6 if gamma is None else gamma
+        self.discount_factior_gamma = 0.5 if gamma is None else gamma
         self.exploration_coeff = 0.1 if expl is None else expl
 
         if self.debug: print(f"Training initiated")
         observation = self.env.reset()
         reward = 0
         penalties = 0
-        for epoch in range(self.EPOCHS): # while having epochs
+        ep = 0
+        stop_condition = True
+        epoch = 0
+        sim_num = 0
+        while(stop_condition): # while having epochs
             #env.render()
-
+            epoch += 1
+            ep += 1
             if np.random.uniform(0, 1) < self.exploration_coeff: #explore
                 action = self.env.action_space.sample()
             else:                                           #exploit
@@ -47,12 +51,19 @@ class QLearn:
 
             observation = new_observation
 
+            if epochs is not None:
+                if epoch == epochs: stop_condition = False
 
             if done:
-                self.simulation_training_log.append((epoch, penalties))
+                self.simulation_training_log.append((ep, penalties))
                 observation = self.env.reset()
                 reward = 0
                 penalties = 0
+                ep = 0
+                sim_num += 1
+
+                if simulations is not None:
+                    if sim_num == simulations: stop_condition = False
 
                 if len(self.simulation_training_log) % 1000 == 0 and self.debug:
                     print(f"Simulation: {len(self.simulation_training_log)} Epochs: {self.simulation_training_log[-1][0]} Penalties: {self.simulation_training_log[-1][1]}")
@@ -100,7 +111,46 @@ class QLearn:
         self.env.close()
 
 
+def plotter(model: QLearn, compact_scale = 1):
+    epochs = np.array(model.simulation_training_log).T[0]
+    penal = np.array(model.simulation_training_log).T[1]
+    if compact_scale != 1:
+        new_epochs = []
+        new_penal = []
+        start = 0
+        while(start + compact_scale < len(model.simulation_training_log)):
+            new_epochs.append(np.mean(epochs[start:(start + compact_scale)]))
+            new_penal.append(np.mean(penal[start:(start+compact_scale)]))
+            start += compact_scale
+        epochs = new_epochs
+        penal = new_penal
+    simulations = len(epochs)
+
+    fig, ax1 = plt.subplots()
+    ax1.set_xlabel(f"Training simulations packs by {compact_scale}")
+    ax1.set_ylabel("Epochs", color='b')
+    ax1.plot(range(simulations), epochs, '-b', label="epochs", markersize=3)
+    ax1.tick_params(axis='y', labelcolor='b')
+
+    ax2 = ax1.twinx()
+    ax2.set_ylabel("Penalties", color='r')
+    ax2.plot(range(simulations), penal, '-r', label="penalties", markersize=3)
+    ax2.tick_params(axis='y', labelcolor='r')
+
+    plt.title(label=f"Alpha = {model.learning_rate_alpha} Gamma = {model.discount_factior_gamma} Exploration = {model.exploration_coeff}")
+    plt.tight_layout()
+    plt.gcf().savefig(f"./Reinforcement-Learning/Charts/Alpha={model.learning_rate_alpha}Gamma={model.discount_factior_gamma}Exploration={model.exploration_coeff}.png", format='png')
+    plt.clf()
+
 if __name__ == "__main__":
-    qmodel = QLearn()
-    qmodel.train()
-    qmodel.evaluate(1000)
+    alphas = [0.1, 0.5, 0.8]
+    gammas = [0.1, 0.5, 0.8]
+    explores = [0.1, 0.5, 0.8]
+
+    for a in alphas:
+        for g in gammas:
+            for e in explores:
+                qmodel = QLearn()
+                qmodel.train(alpha=a, gamma=g, expl=e, simulations=5000)
+                qmodel.evaluate(1000)
+                plotter(qmodel, 100)
